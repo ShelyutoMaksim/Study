e0a29c0e1800ebce3e8c0ffbf0052ce8bb623e54
Maksim_SHelyuto <shelyuto_max@mail.ru>
2022-04-19 15:52:30 +0400

    Exercise with Shops

---
+++ js/ex.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const shoppingMallData = {
+    shops: [
+        {
+            width: 10,
+            length: 5
+        },
+        {
+            width: 15,
+            length: 7
+        },
+        {
+            width: 20,
+            length: 5
+        },
+        {
+            width: 8,
+            length: 10
+        }
+    ],
+    height: 5,
+    moneyPer1m3: 30,
+    budget: 50000
+};
+
+
+function isBudgetEnough() {
+    let coast = 0;
+    shoppingMallData.shops.forEach ((item, i) => {
+        let volumeOfShops = item.length * item.width * shoppingMallData.height * shoppingMallData.moneyPer1m3;
+        coast += volumeOfShops;
+    });
+    if (coast > shoppingMallData.budget) {
+        console.log('Бюджета недостаточно');
+    } else {
+        console.log('Бюджета достаточно');
+    }
+
+}
+isBudgetEnough();
