2ad2a3b2f0f71cd511eee314c4745ec741d5cc54
Maksim_SHelyuto <shelyuto_max@mail.ru>
2022-06-23 15:17:54 +0400

    Exercise 17

--- js/ex17.js
+++ js/ex17.js
@@ -1,1 +1,16 @@
 'use strict';
+
+function amountOfPages(summary) {
+    let str = '';
+    let n = 0;
+    let newNum = 0;
+    while (str.length <= summary - 1) {
+        n++;
+        str += `${n}`;
+        newNum = n;
+    }
+    return newNum;
+
+}
+
+console.log(amountOfPages(1095))
