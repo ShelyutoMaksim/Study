3c5dcdcf36576a01e70bae1f2304d4b7e0e26e3d
Maksim_SHelyuto <shelyuto_max@mail.ru>
2022-07-14 14:17:52 +0400

    Прирост жителей города

--- js/codeWars/town.js
+++ js/codeWars/town.js
@@ -10,4 +10,5 @@ function nbYear(pFirst, percent, aug, pResult) {
 }
 // console.log(nbYear(1500, 5, 100, 5000))
 // console.log(nbYear(1500000, 2.5, 10000, 2000000))
-console.log(nbYear(1500000, 0.25, 1000, 2000000))
+// console.log(nbYear(1500000, 0.25, 1000, 2000000))
+console.log(nbYear(1000, 2, 50, 1200))
